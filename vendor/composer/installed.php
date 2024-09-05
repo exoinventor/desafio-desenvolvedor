87738452c385cc1754a106dc0277f76d041d570a
exoinventor <exoinventor@gmail.com>
2024-09-05 15:05:37 -0300

    mvc init

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'dfa06c63a1a8fb78c4d6ca41725885016d73f529',
+        'name' => 'roleplayinggame/arg',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'roleplayinggame/arg' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'dfa06c63a1a8fb78c4d6ca41725885016d73f529',
+            'dev_requirement' => false,
+        ),
+    ),
+);
